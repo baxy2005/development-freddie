a565883f621dbc11b6027ed4424c461bcb02e0fd
andras varadi <andris@varadi.tv>
2017-01-09 17:44:46 +0100

    setting up front page, navigation header etc.

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,17 +1,32 @@
-$('.owl-carousel').owlCarousel({
+// owl carousel
+$('.main-carousel').owlCarousel({
+	animateOut: 'fadeOut',
+    items:1,
+    lazyLoad:true,
     loop:true,
-    margin:10,
-    nav:true,
-    autoplay: true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:2
-        }
-    }
-})
+  	autoplay: true,
+  	nav:false,
+});
+
+// owl carousel images to the background
+$('.main-carousel .item img').each(function() {
+	var imgSrc = $(this).attr('data-src');
+	$(this).parent().css({'background-image': 'url('+imgSrc+')'});
+	$(this).remove();
+});
+
+
+
+var topoffset = 90;
+
+var isTouch = 'ontouchstart' in document.documentElement;
+
+//window height
+var wheight = $(window).height(); //get height of the window
+
+$('.fullheight').css('height', wheight);
+
+$(window).resize(function() {
+  var wheight = $(window).height(); //get height of the window
+  $('.fullheight').css('height', wheight);
+}) //on resize
